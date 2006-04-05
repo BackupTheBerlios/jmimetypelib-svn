48d445dcfaf099d44901360efcb797f79366a881
pabloa <pabloa@cb93ccbc-9b10-0410-aeb9-bc5413f30746>
2006-04-05 04:38:46 +0000

    initial import

---
+++ trunk/src/main/java/ar/com/pabloa/jmimetypelib/exceptions/ElementCreationException.java
@@ -0,0 +1,42 @@
+/**
+ * jMimetypeLib: a java implementation of Free Desktop Shared MIME-info 
+ * Database specification.
+ * Copyright (C) 2006 Pablo Alcaraz - http://www.dreamsoft.com.ar.
+ * Contact: pabloa@laotraesquina.com.ar
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or any later
+ * version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * Initial developer(s): Pablo Alcaraz.
+ * Contributor(s): 
+ */
+package ar.com.pabloa.jmimetypelib.exceptions;
+
+
+/**
+ * @author pabloa
+ *
+ */
+public class ElementCreationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5856431421343796396L;
+
+	public ElementCreationException(String string, Exception exception) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
